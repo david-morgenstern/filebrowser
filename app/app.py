30a918b8584ea46decc023812aad1bf047c7698f
david-morgenstern <davidyo@protonmail.com>
2023-06-07 15:30:54 +0200

    able to download directory but slowly

--- app/app.py
+++ app/app.py
@@ -1,8 +1,15 @@
+import shutil
+import tempfile
+import io
+import zipfile
+
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import os
+
+from starlette.responses import StreamingResponse
 
 app = FastAPI()
 
@@ -18,10 +25,42 @@ async def download_file(request: Request):
     file_path = form_data["file_path"]
     file_name = form_data["file_name"]
 
-    if not os.path.isfile(file_path):
-        raise HTTPException(status_code=404, detail='File not found')
+    if os.path.isfile(file_path):
+        return FileResponse(file_path, filename=file_name)
+    elif os.path.isdir(file_path):
+        temp_dir = tempfile.mkdtemp()
+        zip_path = os.path.join(temp_dir, file_name + '.zip')
 
-    return FileResponse(file_path, filename=file_name)
+        async def create_zip():
+            print(f"Creating zipfile. {zip_path}")
+            with zipfile.ZipFile(zip_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+                for root, _, files in os.walk(file_path):
+                    for file in files:
+                        file_path_abs = os.path.join(root, file)
+                        relative_path = os.path.relpath(file_path_abs, file_path)
+                        zipf.write(file_path_abs, arcname=relative_path)
+
+        await create_zip()
+
+        def cleanup():
+            print(f"Removing temp_dir. {temp_dir}")
+
+            shutil.rmtree(temp_dir, ignore_errors=True)
+
+        async def stream_zip():
+            with open(zip_path, 'rb') as f:
+                while True:
+                    data = f.read(4096)
+                    if not data:
+                        break
+                    yield data
+            cleanup()
+
+        return StreamingResponse(stream_zip(), media_type='application/zip',
+                                 headers={'Content-Disposition': f'attachment; filename="{file_name}.zip"'})
+
+    else:
+        raise HTTPException(status_code=404, detail='File or directory not found')
 
 
 @app.get('/')
